1112dec4a6f6b18319b7052c742dbb643f836ef5
Evan Roth <eroth0101@gmail.com>
2017-10-24 23:42:36 +0100

    Set up main posts view controller and tableview.

---
+++ BabylonHealthDemo/BabylonHealthDemo/Posts View/PostsTableViewObject.swift
@@ -0,0 +1,22 @@
+//
+//  PostsTableViewObject.swift
+//  BabylonHealthDemo
+//
+//  Created by Evan Roth on 10/24/17.
+//  Copyright © 2017 Evan Roth. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostsTableViewObject: NSObject, UITableViewDelegate, UITableViewDataSource {
+	@IBOutlet weak var postsTableView: UITableView!
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+}
--- BabylonHealthDemo/BabylonHealthDemo/Posts View/PostsViewController.swift
+++ BabylonHealthDemo/BabylonHealthDemo/Posts View/PostsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PostsViewController.swift
 //  BabylonHealthDemo
 //
 //  Created by Evan Roth on 10/23/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PostsViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
