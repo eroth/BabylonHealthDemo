27037d49722013ae57fdfb06284844abc9c7b0dd
Evan Roth <eroth0101@gmail.com>
2017-10-25 13:42:56 +0100

    Few changes.

--- BabylonHealthDemo/BabylonHealthDemo/Posts View/PostsTableViewObject.swift
+++ BabylonHealthDemo/BabylonHealthDemo/Posts View/PostsTableViewObject.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class PostsTableViewObject: NSObject, UITableViewDelegate, UITableViewDataSource {
+class PostsTableViewObject: NSObject {
 	var postsDataSource: [Post] = [] {
 		didSet {
 			postsTableView.reloadData()
@@ -18,24 +18,29 @@ class PostsTableViewObject: NSObject, UITableViewDelegate, UITableViewDataSource
 	
 	@IBOutlet weak var postsTableView: UITableView! {
 		didSet {
-			postsTableView.translatesAutoresizingMaskIntoConstraints = false
-			postsTableView.rowHeight = 130
 			postsTableView.allowsSelection = false
-//			postsTableView.estimatedRowHeight = 100
 			postsTableView.register(UINib.init(nibName: "PostsTableViewCell", bundle: nil), forCellReuseIdentifier: "PostsTableViewCellReuseIdentifier")
 		}
 	}
-	
+}
+
+extension PostsTableViewObject: UITableViewDataSource {
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return postsDataSource.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let postsCell = tableView.dequeueReusableCell(withIdentifier: "PostsTableViewCellReuseIdentifier", for: indexPath) as! PostsTableViewCell
-		let post = postsDataSource[indexPath.row] 
+		let post = postsDataSource[indexPath.row]
 		postsCell.postTitleLabel.text = post.title
 		postsCell.postBodyLabel.text = post.body
 		
 		return postsCell
+	}
+}
+
+extension PostsTableViewObject: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return Constants.MainPostsView.MAIN_POSTS_TABLEVIEWCELL_HEIGHT
 	}
 }
